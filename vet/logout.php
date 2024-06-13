2983a3f18e5701b2f76b68d83489aa4efb19cec5
AbduUSDI <157474429+AbduUSDI@users.noreply.github.com>
2024-06-13 18:15:26 +0200

    Update logout.php

--- vet/logout.php
+++ vet/logout.php
@@ -3,3 +3,5 @@ session_start();
 session_destroy();
 header('Location: index.php');
 exit;
+
+// Fichier de déconnexion permettant de détruire la session pour éviter des problèmes de connexion au site
