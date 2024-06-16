e723e73fd42336cda1d54cf72fd1b6d1e7aa0f4e
AbduUSDI <157474429+AbduUSDI@users.noreply.github.com>
2024-06-16 15:20:08 +0200

    Update add_user.php

--- admin/add_user.php
+++ admin/add_user.php
@@ -1,20 +1,38 @@
 <?php
 session_start();
+require '../functions.php';
+
+// Vérification de l'identification de l'utiliateur, il doit être role 1 donc admin, sinon page login.php
+
 if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] != 1) {
     header('Location: ../login.php');
     exit;
 }
 
-require '../functions.php';
-$conn = dbConnect();
+// Connexion à la base de données
+
+$db = (new Database())->connect();
+
+// Instance User pour utiliser les méthodes en rapport au utilisateurs du zoo
+
+$user = new User($db);
+
+// Traitement et récupération des données du formulaire (POST) d'ajout d'utilisateur
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
+
+    // Hachage du mot de passe pour plus de sécurité
+
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $role_id = $_POST['role_id'];
+    $username = $_POST['username'];
 
-    $stmt = $conn->prepare("INSERT INTO users (email, password, role_id) VALUES (?, ?, ?)");
-    $stmt->execute([$email, $password, $role_id]);
+    // Utilisation de la méthode préparée "addUser" afin d'ajouter le nouvel utilisateur après avoir récupéré toutes les informations fournies
+
+    $user->addUser($email, $password, $role_id, $username);
+
+    // Redirection vers la page de gestion des utilisateurs
 
     header('Location: manage_users.php');
     exit;
@@ -24,12 +42,18 @@ include '../templates/header.php';
 include 'navbar_admin.php';
 ?>
 
+<!-- Conteneur pour afficher le formulaire (POST) pour ajouter un utilisateur -->
+
 <div class="container">
     <h1 class="my-4">Ajouter un Utilisateur</h1>
     <form action="add_user.php" method="POST">
         <div class="form-group">
             <label for="email">Email</label>
             <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" class="form-control" id="username" name="username" required>
         </div>
         <div class="form-group">
             <label for="password">Mot de passe</label>
@@ -43,7 +67,7 @@ include 'navbar_admin.php';
                 <option value="3">Vétérinaire</option>
             </select>
         </div>
-        <button type="submit" class="btn btn-primary">Ajouter</button>
+        <button type="submit" class="btn btn-success">Ajouter</button>
     </form>
 </div>
 
