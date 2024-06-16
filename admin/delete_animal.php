c772c4ad43310f27411d413f82cab8b75a950b69
AbduUSDI <157474429+AbduUSDI@users.noreply.github.com>
2024-06-16 15:19:54 +0200

    Update delete_animal.php

--- admin/delete_animal.php
+++ admin/delete_animal.php
@@ -1,4 +1,7 @@
 <?php
+
+// Vérification de l'identification de l'utiliateur, il doit être role 1 donc admin, sinon page login.php
+
 session_start();
 if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] != 1) {
     header('Location: ../login.php');
@@ -6,11 +9,22 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] != 1) {
 }
 
 require '../functions.php';
-$conn = dbConnect();
+
+// Connexion à la base de donnée
+
+$db = new Database();
+$conn = $db->connect();
+
+// Instance Animal pour utiliser la méthode "delete" afin de supprimer un animal 
+
+$animalManager = new Animal($conn);
+
+// Si l'id n'est pas nulle alors il récupère l'id de l'URL est utilise la méthode "delete"
 
 if (isset($_GET['id'])) {
-    $stmt = $conn->prepare("DELETE FROM animals WHERE id = ?");
-    $stmt->execute([$_GET['id']]);
+    $animalId = $_GET['id'];
+    // Utilisation la méthode préparée "delete" de la classe Animal pour supprimer l'animal
+    $animalManager->delete($animalId);
 }
 
 header('Location: manage_animals.php');
