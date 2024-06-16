1eda63ce9b71dc87c0e496354fb5d7bbe65c6af0
AbduUSDI <157474429+AbduUSDI@users.noreply.github.com>
2024-06-16 15:20:47 +0200

    Update add_animal.php

--- admin/add_animal.php
+++ admin/add_animal.php
@@ -1,4 +1,7 @@
 <?php
+
+// Vérification de l'identification de l'utilisateur, il doit être role 1 donc admin, sinon page login.php
+
 session_start();
 if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] != 1) {
     header('Location: ../login.php');
@@ -6,7 +9,17 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] != 1) {
 }
 
 require '../functions.php';
-$conn = dbConnect();
+
+// Connexion à la base de données
+
+$db = new Database();
+$conn = $db->connect();
+
+// Instance Animal pour utiliser les méthodes en rapport avec les animaux
+
+$animalManager = new Animal($conn);
+
+// Traitement et récupération des données du formulaire (POST) d'ajout d'un animal
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
@@ -14,32 +27,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $habitat_id = $_POST['habitat_id'];
     $image = $_FILES['image'];
 
-    // Vérifiez d'abord si un fichier a été téléchargé
-    if ($image['name']) {
+    // Vérification si un fichier a été téléchargé
+
+    if ($image['error'] == UPLOAD_ERR_OK) {
         $targetDir = "../uploads/";
-        $targetFile = $targetDir . basename($image["name"]);
-        move_uploaded_file($image["tmp_name"], $targetFile);
+        $imageName = time() . '_' . basename($image["name"]);
+        $targetFile = $targetDir . $imageName;
+
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        // Utilisation d'un allowedTypes pour forcer l'utilisateur à utiliser uniquement des fichiers image ou gif
+
+        $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+        if (in_array($fileType, $allowedTypes) && $image['size'] < 5000000) {
+            if (move_uploaded_file($image["tmp_name"], $targetFile)) {
+            } else {
+                $error = "Erreur lors du téléchargement de l'image.";
+            }
+        } else {
+            $error = "Le fichier doit être une image (jpg, jpeg, png, gif) et ne doit pas dépasser 5MB.";
+        }
     } else {
-        // Si aucun fichier n'a été téléchargé, définissez $image sur une valeur par défaut ou NULL, selon votre logique d'application
-        $image = 'chemin_vers_image_par_defaut.jpg'; // Définissez le chemin vers une image par défaut si nécessaire
+        $error = "Erreur de téléchargement de l'image.";
     }
 
-    $stmt = $conn->prepare("INSERT INTO animals (name, species, habitat_id, image) VALUES (?, ?, ?, ?)");
-    $stmt->execute([$name, $species, $habitat_id, $targetFile]); // Utilisez $targetFile comme valeur pour l'image
+    if (!isset($error)) {
 
-    header('Location: manage_animals.php');
-    exit;
+        // Utilisation de la méthode "add" de la classe Animal pour ajouter un nouvel animal
+
+        $animalManager->add([$name, $species, $habitat_id, $targetFile]);
+        header('Location: manage_animals.php');
+        exit;
+    }
 }
 
-$habitats = $conn->query("SELECT * FROM habitats")->fetchAll(PDO::FETCH_ASSOC);
+// Récupération de la liste des habitats pour le formulaire
+
+$habitats = $animalManager->getAllHabitats();
 
 include '../templates/header.php';
 include 'navbar_admin.php';
 ?>
 
+<!-- Conteneur pour afficher le formulaire (POST) pour ajouter un nouvel animal -->
+
 <div class="container">
     <h1 class="my-4">Ajouter un Animal</h1>
-    <form action="add_animal.php" method="POST" enctype="multipart/form-data"> <!-- Ajoutez enctype="multipart/form-data" -->
+    <?php if (isset($error)): ?>
+        <div class="alert alert-danger"><?php echo $error; ?></div>
+    <?php endif; ?>
+    <form action="add_animal.php" method="POST" enctype="multipart/form-data">
         <div class="form-group">
             <label for="name">Nom</label>
             <input type="text" class="form-control" id="name" name="name" required>
@@ -58,9 +95,9 @@ include 'navbar_admin.php';
         </div>
         <div class="form-group">
             <label for="image">Image</label>
-            <input type="file" class="form-control" id="image" name="image" required> <!-- Remplacez le champ de saisie par un champ de téléchargement de fichier -->
+            <input type="file" class="form-control" id="image" name="image" required>
         </div>
-        <button type="submit" class="btn btn-primary">Ajouter</button>
+        <button type="submit" class="btn btn-success">Ajouter</button>
     </form>
 </div>
 
