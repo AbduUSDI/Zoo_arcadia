2a8ed0814e140c7e2e48c63c7941af6947422ea3
AbduUSDI <157474429+AbduUSDI@users.noreply.github.com>
2024-06-16 15:06:05 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,5 @@
+<!-- Utilisation d'un header template pour l'inclure à chaque page afin de faciliter la lecture du code -->>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zoo Arcadia</title>
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Inclusion de Bootstrap 5 -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
 </head>
 <body>
